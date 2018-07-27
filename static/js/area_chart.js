237402eef03c92374c7a5bcb7156861cb6bcc3eb
Sean <yycyjqc@gmail.com>
2018-07-27 16:01:42 -0700

    use tableau instead of Plotly

--- static/js/area_chart.js
+++ static/js/area_chart.js
@@ -1,19 +1,80 @@
-let url = "/test_data";
+let data_url = "/api/bubble_chart";
 
+Plotly.d3.csv(data_url, function(err, data){
 
-var stacksDiv = document.getElementById("myDiv");
-var traces = [
-    {x: [1,2,3], y: [2,1,4], fill: 'tozeroy'},
-    {x: [1,2,3], y: [1,1,2], fill: 'tonexty'},
-    {x: [1,2,3], y: [3,0,2], fill: 'tonexty'}
-];
-function stackedArea(traces) {
-    for(var i=1; i<traces.length; i++) {
-        for(var j=0; j<(Math.min(traces[i]['y'].length, traces[i-1]['y'].length)); j++) {
-            traces[i]['y'][j] += traces[i-1]['y'][j];
+    function unpack(data, key) {
+        return data.map(function(row) { return row[key]; });
+    }
+
+    var allAges = unpack(data, 'age'),
+        allYear = unpack(data, 'year'),
+        allPopulation = unpack(data, 'population'),
+        listsofAges = [],
+        currentAge,
+        currentPopulation = [],
+        currentYear = [];
+
+    for (var i = 0; i < allAges.length; i++ ){
+        if (listsofAges.indexOf(allAges[i]) === -1 ){
+            listsofAges.push(allAges[i]);
         }
     }
-    return traces;
-}
 
-Plotly.newPlot(stacksDiv, stackedArea(traces), {title: 'stacked and filled line chart'});
+    function getAgeData(chosenAge) {
+        currentPopulation = [];
+        currentYear = [];
+        for (var i = 0 ; i < allAges.length ; i++){
+            if ( allAges[i] === chosenAge ) {
+                currentPopulation.push(allPopulation[i]);
+                currentYear.push(allYear[i]);
+            }
+        }
+    };
+
+    // Default Age Data
+    setBubblePlot('18-24');
+
+    function setBubblePlot(chosenAge) {
+        getAgeData(chosenAge);
+
+        var trace1 = {
+            x: currentYear,
+            y: currentPopulation,
+            mode: 'lines+markers',
+            marker: {
+                size: 12,
+                opacity: 0.5
+            }
+        };
+
+        var data = [trace1];
+
+        var layout = {
+            title:'Line and Scatter Plot',
+            height: 600,
+            width: 840
+        };
+
+        Plotly.newPlot('plotdiv', data, layout);
+    };
+
+    var innerContainer = document.querySelector('[data-num="0"'),
+        plotEl = innerContainer.querySelector('.plot'),
+        ageSelector = innerContainer.querySelector('.agedata');
+
+    function assignOptions(textArray, selector) {
+        for (var i = 0; i < textArray.length;  i++) {
+            var currentOption = document.createElement('option');
+            currentOption.text = textArray[i];
+            selector.appendChild(currentOption);
+        }
+    }
+
+    assignOptions(listsofAges, ageSelector);
+
+    function updateAge(){
+        setBubblePlot(ageSelector.value);
+    }
+
+    ageSelector.addEventListener('change', updateAge, false);
+});
